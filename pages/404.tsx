5c4fab1cf6e81334c681ea2ff4248cdb4b2d6b2a
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-04-09 13:30:42 -0400

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Page from "@components/Page";
+
+export default function Custom404() {
+  return (
+    <Page
+      description="We couldn't find the page you're trying to view"
+      title="404: Page Not Found"
+    />
+  );
+}
