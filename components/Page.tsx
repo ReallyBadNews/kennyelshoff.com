77ee34d935805189eeec48b6f47b83e8f34bb2c0
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-06-30 23:23:52 -0400

    chore: rebase package fix
    Signed-off-by: Kenny Elshoff <kelshoff@grahamdigital.com>

--- components/Page.tsx
+++ components/Page.tsx
@@ -11,7 +11,7 @@ import { Text } from "./Text";
 
 export interface PageProps {
   title?: string;
-  description?: string | NULL;
+  description?: string;
   date?: string;
   showDivider?: boolean;
   showHeader?: boolean;
