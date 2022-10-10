32394a9d855410f941fbac1bbc2e7239587b7282
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-10-09 22:42:24 -0400

    Refactor custom document to functionial component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-danger */
 
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import { getCssText, reset } from "stitches.config";
 
 /**
@@ -14,39 +14,38 @@ const getCssAndReset = () => {
   return css;
 };
 
-export default class Document extends NextDocument {
-  render(): JSX.Element {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            as="font"
-            crossOrigin="anonymous"
-            href="/fonts/Inter-var.woff2"
-            rel="preload"
-            type="font/woff2"
-          />
-          <link
-            as="font"
-            crossOrigin="anonymous"
-            href="/fonts/iAWriterQuattroV.woff2"
-            rel="preload"
-            type="font/woff2"
-          />
-          <link
-            as="font"
-            crossOrigin="anonymous"
-            href="/fonts/JetBrainsMono-Regular.woff2"
-            rel="preload"
-            type="font/woff2"
-          />
-          <style
-            dangerouslySetInnerHTML={{ __html: getCssAndReset() }}
-            id="stitches"
-          />
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/fonts/Inter-var.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/fonts/iAWriterQuattroV.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/fonts/JetBrainsMono-Regular.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <style
+          dangerouslySetInnerHTML={{ __html: getCssAndReset() }}
+          id="stitches"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
                 @font-face {
                   font-family: 'iAWriter Quattro';
                   font-weight: 200 900;
@@ -68,14 +67,13 @@ export default class Document extends NextDocument {
                   src: url(/fonts/JetBrainsMono-Regular.woff2) format('woff2');
                 }
               `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
