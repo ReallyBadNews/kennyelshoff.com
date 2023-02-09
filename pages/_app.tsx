897addbebdcf057d45666420fa1da56f3fd49ec9
Kenny Elshoff <github@kennyelshoff.com>
2023-02-08 21:59:43 -0500

    add vercel analytics (#85)
    * add vercel analytics
    
    * remove fathom analytics
    
    Signed-off-by: Kenny Elshoff <github@kennyelshoff.com>
    
    ---------
    
    Signed-off-by: Kenny Elshoff <github@kennyelshoff.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import { Container } from "@components/Container";
 import { Header } from "@components/Header";
-import { useAnalytics } from "@lib/analytics";
 import { Inter, JetBrains_Mono } from "@next/font/google";
 import localFont from "@next/font/local";
+import { Analytics } from "@vercel/analytics/react";
+import { LazyMotion } from "framer-motion";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import type { AppProps as NextAppProps } from "next/app";
-import { LazyMotion } from "framer-motion";
 import dynamic from "next/dynamic";
 import { darkTheme, globalCss } from "../stitches.config";
 
@@ -156,7 +156,6 @@ function BabaBooey({
   pageProps: { session, ...pageProps },
 }: AppProps<{ session?: Session }>): JSX.Element {
   globalStyles();
-  useAnalytics();
 
   return (
     <ThemeProvider
@@ -180,6 +179,7 @@ function BabaBooey({
               >
                 <Header />
                 <Component {...pageProps} />
+                <Analytics />
                 <DynamicFooter />
               </Container>
             </DynamicCommandPalette>
