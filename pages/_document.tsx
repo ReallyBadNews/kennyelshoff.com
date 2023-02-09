337ed88bc2736694ec186fab878244cd491f5473
Kenny Elshoff <github@kennyelshoff.com>
2023-02-08 21:09:23 -0500

    move to pnpm and update packages (#84)
    * move to pnpm and update packages
    
    * fix: pnpm prebuild
    
    * fix: MDX component types
    
    * remove unused type
    
    * remove `reset()` from css init
    
    * downgrade to next@13.0.2
    
    * upgrade to next@13.0.3
    
    * upgrade to next@13.0.4
    
    * upgrade to next@13.0.6

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { getCssText, reset } from "stitches.config";
+import { getCssText } from "../stitches.config";
 
 /**
  * Get the css and reset the internal css representation.
  * This is very *IMPORTANT* to do as the server might handle multiple requests
  * and we don't want to have the css accumulated from previous requests
  */
-const getCssAndReset = () => {
-  const css = getCssText();
-  reset();
-  return css;
-};
+// const getCssAndReset = () => {
+//   const css = getCssText();
+//   reset();
+//   return css;
+// };
 
 export default function Document() {
   return (
@@ -18,7 +18,7 @@ export default function Document() {
       <Head>
         <style
           // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: getCssAndReset() }}
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
           id="stitches"
         />
       </Head>
--- types/index.ts
+++ types/index.ts
@@ -9,5 +9,3 @@ export type MDXImages = {
 };
 
 export type MDXImage = MDXImages[string];
-
-export type ComponentMap = import("mdx/types").MDXComponents;
